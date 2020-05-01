26fbfb8f0962e6597df7e75694209bf059f83fbe
Michał Borowski <m.borowski282@gmail.com>
2020-05-01 15:03:31 +0200

    start added

--- main.js
+++ main.js
@@ -1,13 +1,51 @@
-var jsonString = "{\n\t\"intro\": \"Liczy\u0107 ka\u017Cdy mo\u017Ce.\",\n\t\"questions\": [\n\t\t{ \"text\": \"1 + 2 = ?\", \"answer\":3 },\n\t\t{ \"text\": \"8 + 4 = ?\", \"answer\":12 },\n\t\t{ \"text\": \"7 - 2 = ?\", \"answer\":5 },\n\t\t{ \"text\": \"21 * 37 = ?\", \"answer\":777 }\n\t],\n\t\"points\": \"b\u0142\u0119dna odpowied\u017A na pytanie pierwsze to 4 sekundy, a na drugie to 10 sekund\"\n}";
+var jsonString = "{\n\t\"intro\": \"Liczy\u0107 ka\u017Cdy mo\u017Ce.\",\n\t\"questions\": [\n\t\t{ \"text\": \"1 + 2 = ?\", \"answer\":3, \"penalty\":8 },\n\t\t{ \"text\": \"8 + 4 = ?\", \"answer\":12, \"penalty\":10 },\n\t\t{ \"text\": \"7 - 2 = ?\", \"answer\":5, \"penalty\":7 },\n\t\t{ \"text\": \"21 * 37 = ?\", \"answer\":777, \"penalty\":5 }\n\t]\n}";
 var quiz = JSON.parse(jsonString);
 var curNum = 0;
 var maxNum = quiz.questions.length;
 var answers = new Array(maxNum);
+var times = new Array(maxNum);
+var curTime = 0;
+var lastTime = 0;
+var interval;
 function showQuestion(id) {
     var questionElement = document.getElementById("question");
     var answerElement = document.getElementById("answer");
+    var footerElement = document.getElementById("footer");
     questionElement.innerHTML = quiz.questions[id].text;
     answerElement.value = answers[id];
+    footerElement.innerHTML = "Pytanie : " + (curNum + 1) + " / " + maxNum + "<br>Kara za złą odpowiedź : " + quiz.questions[curNum].penalty + " sekund(y)";
+}
+function disableButtons(id) {
+    var prevButton = document.getElementById("prev");
+    var nextButton = document.getElementById("next");
+    if (id === 0) {
+        prevButton.disabled = true;
+    }
+    else {
+        prevButton.disabled = false;
+    }
+    if (id === maxNum - 1) {
+        nextButton.disabled = true;
+    }
+    else {
+        nextButton.disabled = false;
+    }
+}
+function actTime(id) {
+    times[id] += (curTime - lastTime);
+    lastTime = curTime;
+}
+function nextQuestion() {
+    actTime(curNum);
+    curNum++;
+    showQuestion(curNum);
+    disableButtons(curNum);
+}
+function prevQuestion() {
+    actTime(curNum);
+    curNum--;
+    showQuestion(curNum);
+    disableButtons(curNum);
 }
 function checkAnswers() {
     for (var i = 0; i < maxNum; i++) {
@@ -16,29 +54,86 @@ function checkAnswers() {
     }
     return true;
 }
-function nextQuestion() {
-    console.log("1");
+function changeAnswer() {
     var answerElement = document.getElementById("answer");
     answers[curNum] = answerElement.value;
-    curNum++;
-    showQuestion(curNum);
+    var stopButton = document.getElementById("stop");
+    if (checkAnswers())
+        stopButton.disabled = false;
+    else
+        stopButton.disabled = true;
 }
-function prevQuestion() {
-    var answerElement = document.getElementById("answer");
-    answers[curNum] = answerElement.value;
-    curNum--;
-    showQuestion(curNum);
+function changeTimer() {
+    curTime++;
+    var timerElement = document.getElementById("timer");
+    timerElement.innerHTML = "Minęło " + curTime + " sekund.";
 }
-function init() {
+function getResult(id) {
+    var report = (id + 1) + " : udzielona odpowiedź : " + answers[id];
+    ans = Number(answers[id]);
+    if (ans === quiz.questions[id].answer) {
+        report += '<span class="correct"> poprawna </span>';
+    }
+    else {
+        report += '<span class="incorrect"> niepoprawna </span>, prawidłowa : ' + quiz.questions[id].answer;
+        times[id] += quiz.questions[id].penalty;
+    }
+    report += ", czas : " + times[id];
+    report += "s.<br>";
+    return report;
+}
+function hideQuiz() {
+    document.getElementById("prev").style.display = 'none';
+    document.getElementById("next").style.display = 'none';
+    document.getElementById("stop").style.display = 'none';
+    document.getElementById("footer").style.display = 'none';
+    document.getElementById("timer").style.display = 'none';
+    document.getElementById("answer").style.display = 'none';
+}
+function showQuiz() {
+    document.getElementById("prev").style.display = 'block';
+    document.getElementById("next").style.display = 'block';
+    document.getElementById("stop").style.display = 'block';
+    document.getElementById("footer").style.display = 'block';
+    document.getElementById("timer").style.display = 'block';
+    document.getElementById("answer").style.display = 'block';
+}
+function endQuiz() {
+    clearInterval(interval);
+    hideQuiz();
+    var report = "Wyniki : <br>";
+    for (var i = 0; i < maxNum; i++) {
+        report += getResult(i);
+    }
+    result = 0;
+    for (var i = 0; i < maxNum; i++)
+        result += times[i];
+    report += "Wynik końcowy : " + result + "s.";
+    document.getElementById("question").innerHTML = report;
+}
+function initQuiz() {
     for (var i = 0; i < maxNum; i++) {
         answers[i] = "";
+        times[i] = 0;
     }
     showQuestion(0);
-    var introElement = document.getElementById("intro");
+    var answerElement = document.getElementById("answer");
     var prevButton = document.getElementById("prev");
     var nextButton = document.getElementById("next");
-    introElement.innerHTML = quiz.intro;
+    var stopButton = document.getElementById("stop");
+    answerElement.onchange = changeAnswer;
     prevButton.onclick = prevQuestion;
     nextButton.onclick = nextQuestion;
+    stopButton.onclick = endQuiz;
+    prevButton.disabled = true;
+    stopButton.disabled = true;
+    interval = setInterval(changeTimer, 1000);
 }
-init();
+function loadWindow() {
+    hideQuiz();
+    var introElement = document.getElementById("intro");
+    introElement.innerHTML = quiz.intro;
+    var descriptionElement = document.getElementById("question");
+    descriptionElement.innerHTML = "Liczba pytań : " + maxNum + ".";
+}
+window.onload = loadWindow;
--- main.ts
+++ main.ts
@@ -1,35 +1,78 @@
 interface Question {
 	text: string;
 	answer: number;
+	penalty: number;
 }
 
 interface Quiz {
 	intro: string;
 	questions: Question[];
-	points: string;
 }
 
 let jsonString: string = `{
 	"intro": "Liczyć każdy może.",
 	"questions": [
-		{ "text": "1 + 2 = ?", "answer":3 },
-		{ "text": "8 + 4 = ?", "answer":12 },
-		{ "text": "7 - 2 = ?", "answer":5 },
-		{ "text": "21 * 37 = ?", "answer":777 }
-	],
-	"points": "błędna odpowiedź na pytanie pierwsze to 4 sekundy, a na drugie to 10 sekund"
+		{ "text": "1 + 2 = ?", "answer":3, "penalty":8 },
+		{ "text": "8 + 4 = ?", "answer":12, "penalty":10 },
+		{ "text": "7 - 2 = ?", "answer":5, "penalty":7 },
+		{ "text": "21 * 37 = ?", "answer":777, "penalty":5 }
+	]
 }`;
 
 let quiz: Quiz = JSON.parse(jsonString);
 let curNum: number = 0;
 let maxNum: number = quiz.questions.length;
 let answers = new Array<string>(maxNum);
+let times = new Array<number>(maxNum);
+let curTime = 0;
+let lastTime = 0;
+let interval;
 
 function showQuestion(id) {
 	const questionElement = document.getElementById("question");
 	const answerElement = document.getElementById("answer");
+	const footerElement = document.getElementById("footer");
         questionElement.innerHTML = quiz.questions[id].text;
 	answerElement.value = answers[id];
+	footerElement.innerHTML = "Pytanie : " + (curNum + 1) + " / " + maxNum + "<br>Kara za złą odpowiedź : " + quiz.questions[curNum].penalty + " sekund(y)";
+}
+
+function disableButtons(id) {
+	const prevButton = document.getElementById("prev");
+	const nextButton = document.getElementById("next");
+
+	if (id === 0) {
+		prevButton.disabled = true;
+	}
+	else {
+		prevButton.disabled = false;
+	}
+
+	if (id === maxNum - 1) {
+		nextButton.disabled = true;
+	}
+	else {
+		nextButton.disabled = false;
+	}
+}
+
+function actTime(id) {
+	times[id] += (curTime - lastTime);
+	lastTime = curTime;
+}
+
+function nextQuestion() {
+	actTime(curNum);
+	curNum++;
+	showQuestion(curNum);
+	disableButtons(curNum);
+}
+
+function prevQuestion() {
+	actTime(curNum);
+	curNum--;
+	showQuestion(curNum);
+	disableButtons(curNum);
 }
 
 function checkAnswers() {
@@ -39,34 +82,97 @@ function checkAnswers() {
 	return true;
 }
 
-function nextQuestion() {
-	console.log("1");
+function changeAnswer() {
 	const answerElement = document.getElementById("answer");
 	answers[curNum] = answerElement.value;
-	curNum++;
-	showQuestion(curNum);
+	const stopButton = document.getElementById("stop");
+	if (checkAnswers()) stopButton.disabled = false;
+	else stopButton.disabled = true;
 }
 
-function prevQuestion() {
-	const answerElement = document.getElementById("answer");
-	answers[curNum] = answerElement.value;
-	curNum--;
-	showQuestion(curNum);
+function changeTimer() {
+	curTime++;
+	const timerElement = document.getElementById("timer");
+	timerElement.innerHTML = "Minęło " + curTime + " sekund.";
 }
 
-function init() {
+function getResult(id) {
+	let report : string = (id + 1) + " : udzielona odpowiedź : " + answers[id];
+	ans = Number(answers[id]);
+	if(ans === quiz.questions[id].answer) {
+		report += '<span class="correct"> poprawna </span>';
+	}
+	else {
+		report += '<span class="incorrect"> niepoprawna </span>, prawidłowa : ' + quiz.questions[id].answer;
+		times[id] += quiz.questions[id].penalty;
+	}
+	report += ", czas : " + times[id];
+	report += "s.<br>";
+	return report;
+}
+
+function hideQuiz() {
+	document.getElementById("prev").style.display = 'none';
+	document.getElementById("next").style.display = 'none';
+	document.getElementById("stop").style.display = 'none';
+	document.getElementById("footer").style.display = 'none';
+	document.getElementById("timer").style.display = 'none';
+	document.getElementById("answer").style.display = 'none';
+}
+
+function showQuiz() {
+	document.getElementById("prev").style.display = 'block';
+	document.getElementById("next").style.display = 'block';
+	document.getElementById("stop").style.display = 'block';
+	document.getElementById("footer").style.display = 'block';
+	document.getElementById("timer").style.display = 'block';
+	document.getElementById("answer").style.display = 'block';
+}
+
+function endQuiz() {
+	clearInterval(interval);
+	hideQuiz();
+
+	let report = "Wyniki : <br>";
+	for (let i = 0; i < maxNum; i++) {
+		report += getResult(i);
+	}
+
+	result = 0;
+	for (let i = 0; i < maxNum; i++) result += times[i];
+	report += "Wynik końcowy : " + result + "s."
+
+	document.getElementById("question").innerHTML = report;
+}
+
+function initQuiz() {
 	for (let i = 0; i < maxNum; i++) {
 		answers[i] = "";
+		times[i] = 0;
 	}
 	showQuestion(0);
 
-	const introElement = document.getElementById("intro");
+	const answerElement = document.getElementById("answer");
 	const prevButton = document.getElementById("prev");
 	const nextButton = document.getElementById("next");
+	const stopButton = document.getElementById("stop");
 
-	introElement.innerHTML = quiz.intro;
+	answerElement.onchange = changeAnswer;
 	prevButton.onclick = prevQuestion;
 	nextButton.onclick = nextQuestion;
+	stopButton.onclick = endQuiz;
+	prevButton.disabled = true;
+	stopButton.disabled = true;
+
+	interval = setInterval(changeTimer, 1000);
 }
 
-init();
+function loadWindow() {
+	hideQuiz();
+	const introElement = document.getElementById("intro");
+	introElement.innerHTML = quiz.intro;
+	const descriptionElement = document.getElementById("question");
+	descriptionElement.innerHTML = "Liczba pytań : " + maxNum + ".";
+}
+
+window.onload = loadWindow;
